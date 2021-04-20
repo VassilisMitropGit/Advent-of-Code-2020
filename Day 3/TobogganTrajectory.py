7fefaf0dcb0b4686075ee1a2f14b7fbeaad939ae
Vassilis Mitropoulos <vassilis_mitropoulos@yahoo.gr>
2021-04-20 21:32:44 +0300

    Day 3 Part 1 Solution in Python

---
+++ Day 3/TobogganTrajectory.py
@@ -0,0 +1,24 @@
+trajectory = open("TobogganTrajectory.txt", "r")
+trajectoryLines = trajectory.readlines()
+trajectory.close()
+
+length = len(trajectoryLines[1].strip())
+size = len(trajectoryLines)
+
+treeCount = 0
+reachedFinalRow = False
+column = 0
+row = 0
+print(length)
+
+while not reachedFinalRow:
+    if (column >= length):
+        column = column - length
+    if (trajectoryLines[row].strip()[column] == "#"):
+        treeCount += 1
+    row += 1
+    column += 3
+    if (row == size):
+        reachedFinalRow = True
+
+print(treeCount)
