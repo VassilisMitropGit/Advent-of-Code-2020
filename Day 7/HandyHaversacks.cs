8664f34faff996d10efdcbda9a018c6fc5beb8b5
Vassilis Mitropoulos <vassilis_mitropoulos@yahoo.gr>
2021-04-22 23:08:12 +0300

    .gitignore update so c# projects can be pushed

---
+++ Day 7/HandyHaversacks.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Day_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] regulations = System.IO.File.ReadAllLines(@"HandyHaversacks.txt");
+        }
+    }
+}
