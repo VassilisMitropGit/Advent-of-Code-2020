282be9eef7d7d01d7fa62cfb7107470151ad8761
Vassilis Mitropoulos <vassilis_mitropoulos@yahoo.gr>
2021-04-23 21:53:12 +0300

    Trying to make regex work

--- Day 7/HandyHaversacks.cs
+++ Day 7/HandyHaversacks.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿//used hints for Part 1 from the respective reddit thread:
+//https://www.reddit.com/r/adventofcode/comments/k8a31f/2020_day_07_solutions/?sort=confidence
+
+using System;
 using System.Text.RegularExpressions;
 using System.Linq;
 using System.Collections.Generic;
+using System.Collections;
+using static System.Text.RegularExpressions.RegexOptions;
+
 
 namespace Day_7
 {
@@ -9,7 +15,17 @@ namespace Day_7
     {
         static void Main(string[] args)
         {
-            string[] regulations = System.IO.File.ReadAllLines(@"HandyHaversacks.txt");
+
+            Dictionary<string, int> bags = new Dictionary<string, int>();
+            var regulations = System.IO.File.ReadLines(@"HandyHaversacks.txt").
+                ToDictionary(
+                    line => Regex.Match(line, @"^(\w+ \w+)").Groups[1].Value,
+                    line => line.Contains("no other bags.")
+                        ? Console.WriteLine("zonk!")
+                        : Console.WriteLine("got him")
+                );
+
+            regulations.ToList().ForEach(x => Console.WriteLine(x.Key));
         }
     }
 }
