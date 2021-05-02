dbb02646abafa205fe535f15413862436b74afbc
Vassilis Mitropoulos <vassilis_mitropoulos@yahoo.gr>
2021-05-02 13:50:39 +0300

    Day 12 Part 2 solution using Dart

--- Day12/bin/main.dart
+++ Day12/bin/main.dart
@@ -4,8 +4,10 @@ import 'dart:core';
 void main() {
   var path = "../RainRisk.txt";
   Map<String, int> coordinates = {'x': 0, 'y': 0};
-  int courseIndex = 0;
-  List<String> horizon = ["E", "S", "W", "N"];
+  Map<String, int> waypoint = {'x': 10, 'y': 1};
+  //Part 1
+  //int courseIndex = 0;
+  //List<String> horizon = ["E", "S", "W", "N"];
   String command;
   int commandValue;
 
@@ -15,10 +17,18 @@ void main() {
   for (var line in commands) {
     command = line[0];
     commandValue = int.parse(line.substring(1));
-    if (command == "R" || command == "L") {
-      courseIndex = steerShip(command, commandValue, courseIndex);
+
+    //Part 1
+    // if (command == "R" || command == "L") {
+    //   courseIndex = steerShip(command, commandValue, courseIndex);
+    // } else {
+    //   coordinates = moveShip(command, commandValue, horizon[courseIndex], coordinates);
+    // }
+
+    if (command == "F") {
+      coordinates = moveToWaypoint(commandValue, coordinates, waypoint);
     } else {
-      coordinates = moveShip(command, commandValue, horizon[courseIndex], coordinates);
+      waypoint = adjustWaypoint(command, commandValue, waypoint);
     }
   }
   print(coordinates['x']!.abs() + coordinates['y']!.abs());
@@ -53,4 +63,57 @@ steerShip(String command, int commandVal, int courseIndex) {
 
   courseIndex = (courseIndex + angle) % 4;
   return courseIndex;
+}
+
+adjustWaypoint(String command, int commandVal, Map<String, int> waypoint) {
+  if (command == "N") waypoint.update('y', (value) => value + commandVal);
+  if (command == "S") waypoint.update('y', (value) => value - commandVal);
+  if (command == "E") waypoint.update('x', (value) => value + commandVal);
+  if (command == "W") waypoint.update('x', (value) => value - commandVal);
+
+  //Turn commands
+  if (command == "R" || command == "L") {
+    int angle = 0;
+    if (command == "R") {
+      angle = commandVal ~/ 90;
+    }
+    if (command == "L") {
+      angle = (360 - commandVal) ~/ 90;
+    }
+    switch (angle) {
+      case 1:
+        {
+          var oldX = waypoint['x']!;
+          var oldY = waypoint['y']!;
+          waypoint.update('x', (value) => oldY);
+          waypoint.update('y', (value) => -oldX);
+        }
+        break;
+      case 2:
+        {
+          var oldX = waypoint['x']!;
+          var oldY = waypoint['y']!;
+          waypoint.update('x', (value) => -oldX);
+          waypoint.update('y', (value) => -oldY);
+        }
+        break;
+      case 3:
+        {
+          var oldX = waypoint['x']!;
+          var oldY = waypoint['y']!;
+          waypoint.update('x', (value) => -oldY);
+          waypoint.update('y', (value) => oldX);
+        }
+        break;
+    }
+  }
+
+  return waypoint;
+}
+
+moveToWaypoint(int commandVal, Map<String, int> coord, Map<String, int> waypoint) {
+  coord.update('x', (value) => value + (commandVal * waypoint['x']!));
+  coord.update('y', (value) => value + (commandVal * waypoint['y']!));
+
+  return coord;
 }
