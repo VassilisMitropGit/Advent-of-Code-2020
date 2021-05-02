843fcafc8392abf5939ecd3953691ebf0987b5e0
Vassilis Mitropoulos <vassilis_mitropoulos@yahoo.gr>
2021-05-02 13:00:24 +0300

    Day 12 Part 1 solution using Dart

--- Day12/bin/main.dart
+++ Day12/bin/main.dart
@@ -1,5 +1,56 @@
 import 'dart:io';
+import 'dart:core';
 
 void main() {
-  File input = File("../RainRisk.txt");
+  var path = "../RainRisk.txt";
+  Map<String, int> coordinates = {'x': 0, 'y': 0};
+  int courseIndex = 0;
+  List<String> horizon = ["E", "S", "W", "N"];
+  String command;
+  int commandValue;
+
+  File input = File(path);
+  List<String> commands = input.readAsLinesSync();
+
+  for (var line in commands) {
+    command = line[0];
+    commandValue = int.parse(line.substring(1));
+    if (command == "R" || command == "L") {
+      courseIndex = steerShip(command, commandValue, courseIndex);
+    } else {
+      coordinates = moveShip(command, commandValue, horizon[courseIndex], coordinates);
+    }
+  }
+  print(coordinates['x']!.abs() + coordinates['y']!.abs());
+}
+
+moveShip(String command, int commandVal, String course, Map<String, int> coord) {
+  //move commands
+  if (command == "N" || (command == "F" && course == "N")) {
+    coord.update('y', (value) => value + commandVal);
+  }
+  if (command == "S" || (command == "F" && course == "S")) {
+    coord.update('y', (value) => value - commandVal);
+  }
+  if (command == "E" || (command == "F" && course == "E")) {
+    coord.update('x', (value) => value + commandVal);
+  }
+  if (command == "W" || (command == "F" && course == "W")) {
+    coord.update('x', (value) => value - commandVal);
+  }
+
+  return coord;
+}
+
+steerShip(String command, int commandVal, int courseIndex) {
+  int angle = 0;
+  if (command == "R") {
+    angle = commandVal ~/ 90;
+  }
+  if (command == "L") {
+    angle = (360 - commandVal) ~/ 90;
+  }
+
+  courseIndex = (courseIndex + angle) % 4;
+  return courseIndex;
 }
