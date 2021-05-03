521d60c5059674267f34907f5db9fc62d70d74bd
Vassilis Mitropoulos <vassilis_mitropoulos@yahoo.gr>
2021-05-03 18:24:04 +0300

    Day 14 Part 1 solution using Dart

--- Day14/bin/main.dart
+++ Day14/bin/main.dart
@@ -3,4 +3,39 @@ import 'dart:io';
 void main() {
   File myFile = File("../DockingData.txt");
   List<String> input = myFile.readAsLinesSync();
+  String mask = "";
+  Map<int, BigInt> memory = {};
+  BigInt sum = BigInt.zero;
+
+  for (var line in input) {
+    List<String> parsedLine = line.split(" ");
+
+    if (parsedLine[0] == "mask") {
+      mask = parsedLine[2];
+    } else {
+      int memoryIndex = int.parse(parsedLine[0].replaceAll(new RegExp(r'[^0-9]'), ''));
+      memory[memoryIndex] = applyMask(mask, int.parse(parsedLine[2]));
+    }
+  }
+
+  memory.forEach((key, value) {
+    sum += value;
+  });
+
+  print(sum);
+}
+
+applyMask(String mask, int value) {
+  var data = BigInt.from(value);
+  data = convertMaskAND(mask) & data;
+  data = convertMaskOR(mask) | data;
+  return data;
+}
+
+convertMaskAND(String mask) {
+  return BigInt.parse(mask.replaceAll(new RegExp(r'X'), "1"), radix: 2);
+}
+
+convertMaskOR(String mask) {
+  return BigInt.parse(mask.replaceAll(new RegExp(r'X'), "0"), radix: 2);
 }
